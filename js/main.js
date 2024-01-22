3609c0d2656287e7b65055febde005044892a4ad
AMERAHAMDY24 <amirahh8742@gmail.com>
2024-01-22 22:15:01 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,102 @@
+// sideBar
+
+let sidebarInnerWidth=$(".innnerSideBar").innerWidth();
+$(".sideBar").css("left",-sidebarInnerWidth)
+
+$("#closeIcon").click(function()
+
+{
+    if($(".sideBar").css("left")=="0px")
+    {
+        $(".sideBar").animate({left:-sidebarInnerWidth},500)
+    }
+})
+$(".sideIcon").click(function()
+
+{
+    if($(".sideBar").css("left")!="0px"){
+        $(".sideBar").animate({left:0},500)
+
+    }
+
+})
+//hide side bar
+
+let durationOffset=$("#Duration").offset().top;
+$(window).scroll(function(){
+let windowScroll=$(window).scrollTop()
+    if(windowScroll>durationOffset){
+        $(".sideBar").hide(10)
+    }else{
+        $(".sideBar").show(100)
+
+    }
+})
+
+//scrolling
+
+
+$("a[href^='#]").click(function(e){
+    let Href=e.target.getAttribute("href");
+    let sectionOffset=$(Href).offset().top;
+    $("html,body").animate({scrollTop:sectionOffset},1000)
+})
+
+//section2
+
+
+$(".h1").click(function(){
+    $(".p1").slideToggle(1000)
+    $(".p2").slideUp(1000)
+    $(".p3").slideUp(1000)
+    $(".p4").slideUp(1000)
+
+})
+
+
+$(".h2").click(function(){
+    $(".p2").slideToggle(1000)
+    $(".p1").slideUp(1000)
+    $(".p3").slideUp(1000)
+    $(".p4").slideUp(1000)
+
+
+})
+
+$(".h3").click(function(){
+    $(".p3").slideToggle(1000)
+    $(".p1").slideUp(1000)
+    $(".p2").slideUp(1000)
+    $(".p4").slideUp(1000)
+
+})
+
+$(".h4").click(function(){
+    $(".p4").slideToggle(1000)
+    $(".p1").slideUp(1000)
+    $(".p2").slideUp(1000)
+    $(".p3").slideUp(1000)
+
+})
+
+// COUNT DOWN
+let seconds=document.querySelector("#seconds")
+let decrement=60;
+
+let minutes=document.querySelector("#minutes")
+let decMinutes=55;
+
+let x=setInterval(()=>
+{
+    decrement--;
+    seconds.innerHTML=decrement +" s";
+
+    if(decrement==0){
+     decrement=60;
+    minutes.innerHTML= decMinutes-1 +"m"
+decMinutes--
+}
+
+
+},100)
+//input
